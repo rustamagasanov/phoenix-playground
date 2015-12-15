62c45ce4a093a63f1100c84377cb7b8052cb81bc
Rustam Gasanov <rustamagasanov@gmail.com>
2015-12-15 22:50:02 +0100

    Flashes

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,6 +2,9 @@ defmodule PhoenixTestApp.PageController do
   use PhoenixTestApp.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    conn
+      |> put_flash(:notice, "Welcome to Phoenix, from a flash notice!")
+      |> put_flash(:error, "Let's pretend we have an error.")
+      |> render "index.html"
   end
 end
