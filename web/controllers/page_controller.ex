daac650f8e3a14f269b09a9c7c1cc29fd04c79e2
Rustam Gasanov <rustamagasanov@gmail.com>
2016-01-07 16:54:36 +0100

    Built-in flash messages

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -3,8 +3,8 @@ defmodule PhoenixTestApp.PageController do
 
   def index(conn, _params) do
     conn
-      |> put_flash(:notice, "Welcome to Phoenix, from a flash notice!")
-      |> put_flash(:error, "Let's pretend we have an error.")
-      |> render "index.html"
+    |> put_flash(:info, "Welcome to Phoenix, from a flash notice!")
+    |> put_flash(:error, "Let's pretend we have an error.")
+    |> render "index.html"
   end
 end
